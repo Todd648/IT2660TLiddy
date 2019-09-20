38de3daa90f4a71639b78739de6235dcf70bf659
Todd Liddy <Toddj.liddy@gmail.com>
2019-09-20 14:06:58 -0400

    Friday Afternoon 2

--- Week3/Queue/src/com/company/Main.java
+++ Week3/Queue/src/com/company/Main.java
@@ -3,6 +3,31 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+    Queue q = new Queue(3);
+    Listing l;
+    Listing l1 = new Listing("Todd", "1234 Ash St", "1234567890");
+    Listing l2 = new Listing("Roz", "1234 Ash St", "2345678901");
+    Listing l3 = new Listing("Wes", "1234 Ash St", "3456789012");
+    Listing l4 = new Listing("Gizmo", "1234 Ash St", "4567890123");
+
+
+    System.out.println(q.deque());
+
+    System.out.println(q.enque(l1));
+    System.out.println(q.enque(l2));
+    System.out.println(q.enque(l3));
+    System.out.println(q.enque(l4));
+    q.showAll();
+
+    l=q.deque();
+    System.out.println(l.toString());
+    l=q.deque();
+    System.out.println(l.toString());
+    l=q.deque();
+    System.out.println(l.toString());
+    l=q.deque();
+
+    System.out.println(l);
+    System.exit(0);
     }
 }
